8b2a5072d2b76c280a1c4cc85b4f6b9762588a72
Rodrigo Costa <88461161+KaliRodri@users.noreply.github.com>
2021-08-21 18:35:11 +0000

    Add javascript functions

---
+++ script.js
@@ -0,0 +1,7 @@
+function Onclick(){
+         if (document.getElementById("email").value == "")
+            alert("Preencha o email!")
+        if (document.getElementById("pass").value == ""){
+            alert("Preencha a senha!");
+        }
+}
